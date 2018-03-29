5e4898d47e10d994f9a3c756847b79e3c7b785d5
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-29 18:33:56 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  #attr_reader :brand
 
   BRANDS = []
 
@@ -14,7 +14,8 @@ class Shoe
   end
 
   def brand
-    BRANDS << @brand
+    @brand = brand
+    BRANDS << brand
   end
 
 end
